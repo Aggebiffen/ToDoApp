8aefc3426cf8618f3b6ab42451655eee7c73d370
Aggebiffen <aggebrusback@gmail.com>
2023-12-06 14:19:59 +0100

    second commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,6 +2,9 @@ package se.lexicon;
 
 public class Main {
     public static void main(String[] args) {
-
+    Person august = new Person(1,"august", "brusbäck","hej123@gmail.com");
+        System.out.println(august.getPersonInfo());
+        Person daniel = new Person(2,"Daniel", "Carlsson","hej1234@gmail.com");
+        System.out.println(daniel.getPersonInfo());
     }
 }
--- src/main/java/se/lexicon/Person.java
+++ src/main/java/se/lexicon/Person.java
@@ -1,11 +1,50 @@
 package se.lexicon;
 
 public class Person {
-   int id;
-   String firstName;
-   if (firstName) throw new IllegalArgumentException("")
-   String lastName;
-   String email;
+  private int id;
+  private String firstName;
+  private String lastName;
+  private String email;
 
+  public Person (int id,String firstName, String lastName, String email){
+     setFirstname(firstName);
+     setLastName(lastName);
+     setEmail(email);
+     setId(id);
+  }
 
+   public void setFirstname (String firstName){
+      if (firstName == null) throw new IllegalArgumentException("First name was null");
+      this.firstName =firstName;
+   }
+   public String getFirstName() {
+      return firstName;
+   }
+   public void setLastName (String lastName) {
+      if (lastName == null) throw new IllegalArgumentException("Lastname was null");
+      this.lastName =lastName;
+   }
+   public String getLastName() {
+      return lastName;
+   }
+   public int getId() {
+      return id;
+   }
+   public void setId(int id) {
+      if (id <0) {
+          throw new IllegalArgumentException("Id cannot be negative");
+      }
+      this.id = id;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+   public void setEmail(String email) {
+      if (email == null) throw new IllegalArgumentException("email was null");
+     this.email = email;
+   }
+   public String getPersonInfo(){
+      return "{ " + firstName + " " + lastName + ", email: " + email + ", id: " + id + " }";
+   }
 }
