f7be4f4047f4ce1e8c0931778d38dae1cf9086a1
Aggebiffen <aggebrusback@gmail.com>
2023-12-09 16:25:20 +0100

    Final commit

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -6,12 +6,17 @@ public class Main {
     public static void main(String[] args) {
     Person august = new Person ("August", "Brusbäck", "aggebrusback@gmail.com");
     Person daniel = new Person("Daniel", "Carlsson", "hej123@gmail.com");
-       ToDoItem item = new ToDoItem("Chores", "Do the dishes", LocalDate.now(), august);
-        ToDoItem item1 = new ToDoItem("Chores", "Do the dishes", LocalDate.now(), daniel);
+    ToDoItem item = new ToDoItem("Chores", "Do the dishes", LocalDate.now(), august);
+    ToDoItem item1 = new ToDoItem("Chores", "Do the dishes", LocalDate.now(), daniel);
+    ToDoItemTask itemTask = new ToDoItemTask(item, august);
+    ToDoItemTask itemTask2 = new ToDoItemTask(item1, daniel);
+
+
         System.out.println(item.getSummary());
-        System.out.println(august.getSummary());
-       System.out.println(item1.getSummary());
-        System.out.println(daniel.getSummary());
-        //ToDoItemTask itemTask = new ToDoItemTask(daniel, item)
+        //System.out.println(august.getSummary());
+        //System.out.println(item1.getSummary());
+        //System.out.println(daniel.getSummary());
+        System.out.println(itemTask.getSummary());
+        //System.out.println(itemTask2.getSummary());
     }
 }
